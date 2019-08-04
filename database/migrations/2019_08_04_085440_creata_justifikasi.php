285b0ba0b55c568176584607e06bb889d7c25939
Md Ridzuan bin Mohammad Latiah <wanlatiah@gmail.com>
2019-08-04 10:31:24 +0800

    create migration justifikasi

---
+++ database/migrations/2019_08_04_085440_creata_justifikasi.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreataJustifikasi extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('justifikasi', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('basedept_id');
+            $table->datetime('tarikh');
+            $table->text('keterangan');
+            $table->string('flag_kelulusan')->default('MOHON');
+            $table->integer('pelulus_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('justifikasi');
+    }
+}
