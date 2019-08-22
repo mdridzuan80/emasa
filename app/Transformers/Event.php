c010ff99ffaa2847a9373caf273fc5c39914ff8d
Md Ridzuan bin Mohammad Latiah <wanlatiah@gmail.com>
2019-08-22 11:10:32 +0800

    initial refactor subtitle calendar

--- app/Transformers/Event.php
+++ app/Transformers/Event.php
@@ -3,57 +3,77 @@
 namespace App\Transformers;
 
 use App\Utility;
-use Carbon\Carbon;
 use App\Kehadiran;
+use Carbon\Carbon;
 use League\Fractal\TransformerAbstract;
 
 class Event extends TransformerAbstract
 {
+    private $event;
+
+    private $startTag = '<div>';
+
+    private $endTag = '</div>';
+
     public function transform($event)
     {
+        $this->event = $event;
+
         return [
-            'title' => $this->getTitle($event),
+            'title' => $this->getTitle(),
             'start' => $event['start'],
             'end' => $event['end'],
             'allDay' => ($event['allDay'] === 'true') ? true : false,
-            'color' => $this->colorTS($event),
+            'color' => $this->colorTS(),
             'textColor' => $event['textColor'],
             'scheme_id' => $event['id'],
             'scheme' => $event['table_name'],
             'kesalahan' => $event['kesalahan'] ?? json_encode([]),
             'tatatertib_flag' => $event['tatatertib_flag'] ?? Kehadiran::FLAG_TATATERTIB_CLEAR,
-            'textEscape' => false,
         ];
     }
 
-    public function colorTS($event)
+    public function colorTS()
     {
-        if (isset($event['tatatertib_flag']) && $event['tatatertib_flag'] == Kehadiran::FLAG_TATATERTIB_TUNJUK_SEBAB) {
+        if (isset($this->event['tatatertib_flag']) && $this->event['tatatertib_flag'] == Kehadiran::FLAG_TATATERTIB_TUNJUK_SEBAB) {
             return 'Red';
         }
 
-        return $event['color'];
+        return $this->event['color'];
     }
 
     private function getTitle($event)
     {
-        if ($event['table_name'] == 'final') {
-            if ($event['tatatertib_flag'] == Kehadiran::FLAG_TATATERTIB_TUNJUK_SEBAB && !$event['cuti']) {
-                $checkin = $event['check_in'] ? '<div>IN:' . Carbon::parse($event['check_in'])->format('g:i:s A') . '</div>' : '<div><img src="' . asset('images/icons/exclamation.png') . '"/>IN:-</div>';
+        if ($this->event['table_name'] == 'final') {
+            if ($this->event['tatatertib_flag'] == Kehadiran::FLAG_TATATERTIB_TUNJUK_SEBAB && !$this->event['cuti']) {
+                $checkin = $this->subTitleCheckin(asset('images/icons/exclamation.png'));
 
-                if (Utility::kesalahanCheckOut($event['kesalahan']) == Kehadiran::FLAG_KESALAHAN_AWAL) {
-                    $checkout = '<div><img src="' . asset('images/icons/exclamation.png') . '"/>OUT:' . Carbon::parse($event['check_out'])->format('g:i:s A') . '</div>';
+                if (Utility::kesalahanCheckOut($this->event['kesalahan']) == Kehadiran::FLAG_KESALAHAN_AWAL) {
+                    $checkout = '<div><img src="' . asset('images/icons/exclamation.png') . '"/>OUT:' . Carbon::parse($this->event['check_out'])->format('g:i:s A') . '</div>';
                 } else {
-                    $checkout = $event['check_out'] ? '<div>OUT:' . Carbon::parse($event['check_out'])->format('g:i:s A') . '</div>' : '<div><img src="' . asset('images/icons/exclamation.png') . '"/>OUT:-</div>';
+                    $checkout = $this->event['check_out'] ? '<div>OUT:' . Carbon::parse($this->event['check_out'])->format('g:i:s A') . '</div>' : '<div><img src="' . asset('images/icons/exclamation.png') . '"/>OUT:-</div>';
                 }
             } else {
-                $checkin = $event['check_in'] ? '<div>IN:' . Carbon::parse($event['check_in'])->format('g:i:s A') . '</div>' : '<div>IN:-</div>';
-                $checkout = $event['check_out'] ? '<div>OUT:' . Carbon::parse($event['check_out'])->format('g:i:s A') . '</div>' : '<div>OUT:-</div>';
+                $checkin = $this->event['check_in'] ? '<div>IN:' . Carbon::parse($this->event['check_in'])->format('g:i:s A') . '</div>' : '<div>IN:-</div>';
+                $checkout = $this->event['check_out'] ? '<div>OUT:' . Carbon::parse($this->event['check_out'])->format('g:i:s A') . '</div>' : '<div>OUT:-</div>';
             }
 
             return $checkin . $checkout;
         }
 
         return $event['title'];
+    }
+
+    private function subTitleCheckin($icon = '')
+    {
+        if ($this->event['check_in']) {
+            return $this->startTag . 'IN:' . Carbon::parse($this->event['check_in'])->format('g:i:s A') . $this->endTag;
+        }
+
+        if ($icon) {
+            return $this->startTag . '<img src="' . $icon . '"/>IN:-' . $this->endTag;
+        }
+
+        return $this->startTag . 'IN:-' . $this->endTag;
     }
 }
